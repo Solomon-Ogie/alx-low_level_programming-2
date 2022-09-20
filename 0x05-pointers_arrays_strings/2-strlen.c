6e7f6e2b7e6c21a5f4d5610b731150c0c750b412
Dopezayne <zainabaminu100@gmail.com>
2022-09-20 03:41:56 -0700

    Task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - length of a string
+ * @s: a pointer to an int that will be changed/updated
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+	int n;
+
+	n = 0;
+	while (s[u] != '\0')
+	{
+		u++;
+	}
+	return (u);
+}
