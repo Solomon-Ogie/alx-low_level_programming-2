86d33a6133cbcdb5248016918216b7f892a57167
Dopezayne <zainabaminu100@gmail.com>
2022-09-21 11:23:42 -0700

    Task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,12 +9,12 @@
  *   if str1 == str2, 0.
  *   if str1 > str2, the positive difference of the first unmatched char
  */
-int _strncmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	while (*s1 && *s2 && *s1 == *s2)
 	{
 		s1++;
 		s2++;
 	}
-	return (*s1 - *s2);
+	return (*s1 = *s2);
 }
