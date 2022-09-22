73c0d24fa4c3bfa3472dc438aea479e6005e733f
Dopezayne <zainabaminu100@gmail.com>
2022-09-22 10:18:23 -0700

    Task11

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,4 +2,8 @@
 #define MAIN_H
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
+void print_number(int n);
+void print_buffer(char *b, int size);
+void *infinite_add(char *n1, char *n2, char *r, int size_r);
+void *add_strings(char *n1, char *n2, char *r, int r_index);
 #endif
