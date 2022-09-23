1c47edd3921f6340576547c9eb2d38d05fe04315
Dopezayne <zainabaminu100@gmail.com>
2022-09-23 08:54:22 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet
+ *
+ * Return: success
+ */
+
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
