5351ca59f660b9d6c11751e39b9c313cbce591a0
Dopezayne <zainabaminu100@gmail.com>
2022-09-23 06:33:34 -0700

    Task0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * main - prints positive or negative integer
- * Return: 0
+ *main - prints positive or negative integer
+ *Return: 0
  *
  */
 int main(void)
@@ -11,5 +11,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
