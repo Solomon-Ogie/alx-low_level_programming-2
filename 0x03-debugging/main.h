7a23c8b277146306c263304e1de2a1449c52647b
Dopezayne <zainabaminu100@gmail.com>
2022-09-24 02:24:43 -0700

    Task 0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,10 +3,9 @@
 
 #include <stdio.h>
 
-void_positive_or_negative(int i)
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
+void positive_or_negative(int i);
+int largest_analysis(int, int, int);
+void print_remianing_days(int, int, int);
+int covert_day(int, int);
 
-#endif/*MAIN_H*/
-
+#endif
