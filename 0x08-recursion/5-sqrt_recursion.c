318722f061c67ee6910bd4b379935e9d50b33b4a
Dopezayne <zainabaminu100@gmail.com>
2022-09-28 04:55:21 -0700

    Task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -13,6 +13,23 @@ int _evaluate(int i, int n)
 		return (_evaluate(i + 1, n));
 	else if (i * i == n)
 		return (i);
-	        return (-1);
-	        return (-1);
+	return (-1);
+		return (-1);
 }
+/**
+ * _sqrt_recursion - evaluate sqrt
+ * @n: integer
+ * Return: sqrt_recursion
+ */
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (i < 0)
+		return (-1);
+	else
+	{
+		return (_evaluate(i, n));
+	}
+}
+
