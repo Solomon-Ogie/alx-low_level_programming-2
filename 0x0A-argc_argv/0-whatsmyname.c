3a708d8bd825513cd8eaf67cad8542811f71dec5
Dopezayne <zainabaminu100@gmail.com>
2022-09-30 12:44:12 -0700

    Task0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,16 +2,22 @@
 #include "main.h"
 
 /**
- * main - writes a program that prints its name
- * if you rename the program, it will print the new name
- * compile it again
- * @argc: this is the argument count
- * @argv: this is the argument vector
- * Return: this returns to 0
+ * main - print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: 0 (Success)
  */
+
 int main(int argc, char *argv[])
+
 {
-	printf("%s\n", argv[0]);
-	(void)argc:
-	return (0);
+
+		(void) argc;
+
+			printf("%s\n", argv[0]);
+
+
+
+				return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,2 +1,13 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+
+
 int _putchar(char c);
-int main(int argc, char *argv[]);
+
+int check_num(char *str);
+
+
+
+#endif
