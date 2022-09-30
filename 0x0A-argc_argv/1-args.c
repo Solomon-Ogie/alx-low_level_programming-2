765cb41e9a6783370dc24c3f0e411e87652606f4
Dopezayne <zainabaminu100@gmail.com>
2022-09-30 12:48:11 -0700

    Task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print the name of the program
+ * @argc: Count the arguments
+ * @argv: Arguments
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+
+{
+
+		(void) argv;
+
+			printf("%i\n", argc - 1);
+
+
+
+				return (0);
+
+}
