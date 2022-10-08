4969f928736829f0f31a354f4715605e178e4645
Dopezayne <zainabaminu100@gmail.com>
2022-10-07 17:49:01 -0700

    Task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *malloc_checked - array that prints a string
+ *
+ * @b: num of memory
+ *
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+
+	return (p);
+}
