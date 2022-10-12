63f0fe60d4a7c06288410b09c08eac76fd14ba68
Dopezayne <zainabaminu100@gmail.com>
2022-10-12 13:28:46 -0700

    Task3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -22,11 +22,22 @@ int main(int argc, char *argv[])
 	arg2 = atoi(argv[3]);
 
 	func = get_op_func(argv[2]);
+
+	if (!func)
 	{
 		printf("Error\n");
 		exit(99);
 	}
+	o = *argv[2];
+
+	if ((o == '/' || o == '%') && arg2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	result = func(arg1, arg2);
+
 	printf("%d\n", result);
+
 	return (0);
 }
