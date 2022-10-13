4120bd6a34e7d4146f1d712b793c5ddf04272b25
Dopezayne <zainabaminu100@gmail.com>
2022-10-13 05:13:41 -0700

    Task1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -8,7 +8,7 @@
  * @n: number of integers passed to the function
  * Return: void
  */
-void print_numbers(const char *separator, const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list numbers;
